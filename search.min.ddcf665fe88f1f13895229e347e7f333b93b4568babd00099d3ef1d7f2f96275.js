20cc80aa48fe00393a49fc4c632a4d1d6d08c040
zengyunlong <273412935@qq.com>
2019-10-29 13:46:53 +0800

    init

---
+++ search.min.ddcf665fe88f1f13895229e347e7f333b93b4568babd00099d3ef1d7f2f96275.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/vnpy-handbook/flexsearch.min.js');loadScript('/vnpy-handbook/search-data.min.c8d3cd94b22374d7eb80915a8d2e5561d70f84a78c39e269e2aa7ad018a3c08a.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
